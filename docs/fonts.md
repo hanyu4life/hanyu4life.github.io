93c5cb7e4191e6a6b38b808eaf906d053956af55
hahs <hahs@vng.com.vn>
2025-02-02 12:35:49 +0700

    clean old html

--- docs/fonts.md
+++
@@ -1,2 +0,0 @@
-https://www.jsdelivr.com/package/npm/cn-fontsource-fz-kai-z-03-regular
-https://cdn.jsdelivr.net/npm/cn-fontsource-fz-kai-z-03-regular/
--- scripts/join_words.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const allLines = fs.readFileSync('../data/words.txt', { encoding: 'utf-8' });
-const joinWords = allLines.split('\n').map(t => '\'' + t + '\'').join(',');
-fs.writeFileSync('../output/join_words.txt', joinWords, { encoding: 'utf-8' });
-console.log('DONE');
